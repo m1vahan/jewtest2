f4d0f02c2904c9051d4921cc10277157790b9bac
m1vahan <m1vahan@gmail.com>
2024-10-27 01:16:22 +0400

    scrypt.js

--- scrypt.js
+++ scrypt.js
@@ -1,24 +1,28 @@
-const viewer = new CoreViewerApp({
-    canvas: document.getElementById("mcanvas")
-});
+import { ViewerApp, AssetManagerPlugin, GBufferPlugin, ProgressivePlugin, TonemapPlugin, SSRPlugin, SSAOPlugin, BloomPlugin, GammaCorrectionPlugin, addBasePlugins } from "@webgi/core";
 
-viewer
-    .initialize({
-        caching: true,
-        ground: true,
-        bloom: true,
-        enableDrop: false
-    })
-    .then((viewer) => {
-        // Set the quality based on display
-        viewer.renderManager.displayCanvasScaling = window.devicePixelRatio;
-
-        const loadingScreen = viewer.getPlugin(LoadingScreenPlugin);
-        loadingScreen.loadingTextHeader = "Loading Custom 3D Experience";
-        loadingScreen.showFileNames = false;
-
-        // Load a model
-        const model = "https://demo-assets.pixotronics.com/pixo/gltf/earringScene2.glb";
-        viewer.load(model);
-        console.log(viewer);
+async function setupViewer() {
+    const viewer = new ViewerApp({
+        canvas: document.getElementById("mcanvas")
     });
+
+    await addBasePlugins(viewer);
+
+    const manager = await viewer.addPlugin(AssetManagerPlugin);
+
+    // Add some plugins
+    await viewer.addPlugin(GBufferPlugin);
+    await viewer.addPlugin(new ProgressivePlugin(32));
+    await viewer.addPlugin(new TonemapPlugin(true));
+    await viewer.addPlugin(GammaCorrectionPlugin);
+    await viewer.addPlugin(SSRPlugin);
+    await viewer.addPlugin(SSAOPlugin);
+    await viewer.addPlugin(BloomPlugin);
+
+    // Load a model
+    const model = "https://demo-assets.pixotronics.com/pixo/gltf/earringScene2.glb";
+    await manager.addFromPath(model);
+
+    viewer.renderer.refreshPipeline();
+}
+
+setupViewer();
